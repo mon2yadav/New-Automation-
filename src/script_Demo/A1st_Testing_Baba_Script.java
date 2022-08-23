77834554b670f095cfeea8e9de9f0cc53d553211
montu yadav <mon2yadav@gmail.com>
2022-08-23 15:14:51 +0530

    first commit

---
+++ src/script_Demo/A1st_Testing_Baba_Script.java
@@ -0,0 +1,26 @@
+package script_Demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A1st_Testing_Baba_Script {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.testingbaba.com");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.findElement(By.xpath("//*[@id=\"myModal2\"]/div/div/div[1]/button")).click();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+//		driver.close();
+		
+	}
+
+}
